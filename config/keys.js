bcac1b9b9fb7854b7698f8ed3d09da9b05df7217
ausi1997.com <ausafen1018@gmail.com>
2021-06-21 08:46:29 +0530

    google authentication

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    googleClientID:"975349080195-h8en4jn7sqe0edf2o5vpfk27pgnahq54.apps.googleusercontent.com",
+    googleClientSecret:"8qA_ELcYy4uiNFQIz5l2A_Yt"
+}
--- index.js
+++ index.js
@@ -3,16 +3,42 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+const keys = require('./config/keys'); // importing the keys
 
 // making the express app
 const app = express();
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 8000;
 
 // default route
 app.get('/',(req,res)=>{
     res.send('hello world');
 });
+
+// authentication via google strategy
+
+passport.use(new GoogleStrategy({
+    clientID:keys.googleClientID,
+    clientSecret:keys.googleClientSecret,
+    callbackURL:"/auth/google/callback"
+},
+  function(accessToken , refreshToken , profile , done){
+      console.log("access token" , accessToken);
+      console.log("refresh token" , refreshToken);
+      console.log("profile" , profile);
+      console.log("done" , done);
+  }
+))
+
+// callback url
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+// google sigin route
+
+app.get('/auth/google' , passport.authenticate('google',{
+    scope:['profile', 'email']    // scope is for what you want from the googlr account
+}));
 
 // assigning the port no.
 app.listen(PORT,()=>{
